89d308d435b75d21ca9abcd87d74c5ea3b478227
ndaly06 <ndaly06@qub.ac.uk>
2018-10-23 22:22:52 +0100

    making statements section started

---
+++ src/makingstatements/If.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package makingstatements;
+
+/**
+ * @author nialdaly
+ *
+ */
+public class If {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
